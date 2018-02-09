ce201f2b8c12b50d749b167c897f7e2ea559b8c2
Mateusz Ch <Chl3bak05@users.noreply.github.com>
2018-02-10 00:03:16 +0100

    V0.1
    Added
    -Friction
    -Wall stuck bug fix
    -Animation Speed scale
    -Minor bug and code clarity fixes

--- New Unity Project/Assets/Scripts/Check_Grounded.cs
+++ New Unity Project/Assets/Scripts/Check_Grounded.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Check_Grounded : MonoBehaviour {
 
-    public Anim_Player Anim_Player;
+    private Player player;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        Anim_Player.player = gameObject.GetComponentInParent<Player>();
+        player = gameObject.GetComponentInParent<Player>();
 	}
     //Sending grounded value to Anim_Player
     void OnTriggerEnter2D(Collider2D col)
     {
-        Anim_Player.player.grounded = true;
+        player.grounded = true;
     }
 
     private void OnTriggerStay2D(Collider2D col)
     {
-        Anim_Player.player.grounded = true;
+        player.grounded = true;
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
-        Anim_Player.player.grounded = false;
+        player.grounded = false;
     }
 
     // Update is called once per frame
--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float max_speed = 3;
-    public float speed = 50f;
+    private float speed = 50f;
     public float jump_power = 300f;
+    public float friction_value = 0.8f;
 
     public bool grounded;
 
     public Rigidbody2D rb2d_player;
 
+    public float velocity_x;
+
     // Use this for initialization
     void Start ()
     {
         rb2d_player = gameObject.GetComponent<Rigidbody2D>();
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Setting Horizontal velocity
+        velocity_x = Mathf.Abs(rb2d_player.velocity.x);
         //Limiting speed
         if (Input.GetAxis("Horizontal") < -0.1f)
         {
@@ -33,7 +39,7 @@ public class Player : MonoBehaviour {
         }
 
         //Jumping
-        if (Input.GetButtonDown("Jump") && grounded != false)
+        if (Input.GetButtonDown("Jump") && grounded)
         {          
                 rb2d_player.AddForce(Vector2.up * jump_power);           
         }
@@ -41,6 +47,19 @@ public class Player : MonoBehaviour {
 
     void FixedUpdate()
     {
+        //Fake friction
+        Vector3 ease_Velocity = rb2d_player.velocity;
+        ease_Velocity.x *= friction_value;
+        ease_Velocity.y = rb2d_player.velocity.y;
+        ease_Velocity.z = 0.0f;
+
+        if (grounded)
+        {
+            rb2d_player.velocity = ease_Velocity;
+        }
+
+
+
         // Getting direction horizontal
         float h = Input.GetAxis("Horizontal");
 
